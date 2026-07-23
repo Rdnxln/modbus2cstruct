1c5bfa57953b2e9cd2ff2306131309c301c0a106
Alexander Demin <sasha__d@inbox.ru>
2026-07-23 23:41:57 +0300

    Оформление кода

--- modbus_reqs.c
+++ modbus_reqs.c
@@ -8,41 +8,26 @@ int compare_regs( const void *i1, const void *i2 )
 {
   uint16_t reg1 = *( const uint16_t *)i1;
   uint16_t reg2 = *( const uint16_t *)i2;
-
-  /*
-  return ( (int)reg1 - (int)reg2 );
-   */
-
+  /* return ( (int)reg1 - (int)reg2 ); */
   if( reg1 < reg2 ) return -1;
   if( reg1 > reg2 ) return  1;
 
   return 0;
 }
 
-/* Оптимальная группировка задействованных адресов в запросы Modbus
-   Для HR-, IR- регистров
-   regs          Исходный отсортированный массив адресов
-   regs_count    Количество элементов в массиве regs
-   out_requests  Выходной массив для структуры запросов
-   gap_in_regs   Интервал-"дыра" из ненужных регистров между нужными регистрами (для разделения/объединения запросов)
-   Результат:    Количество сформированных запросов */
-int optimize_modbus_requests( uint16_t         *regs,
-                              uint16_t          regs_count,
-                              modbus_request_t *out_requests,
-                              uint16_t          gap_in_regs )
+/* Оптимальная группировка задействованных адресов в запросы Modbus */
+int optimize_modbus_requests( uint16_t         *regs,         /* Исходный отсортированный массив адресов */
+                              uint16_t          regs_count,   /* Количество элементов в массиве regs */
+                              modbus_request_t *out_requests, /* Выходной массив для структуры запросов */
+                              uint16_t          gap_in_regs ) /* Интервал-"дыра" из ненужных регистров между нужными  */
 {
-
   uint16_t  gap_in_regs_opt = gap_in_regs;
   if( regs_count == 0 ) return 0;
 
   qsort( regs, regs_count, sizeof(uint16_t), compare_regs );
 
-  if( gap_in_regs < MAX_RTU_GAP ) {
-    gap_in_regs_opt = MAX_RTU_GAP;
-  }
-  else if( gap_in_regs > MAX_TCP_GAP ) {
-    gap_in_regs_opt = MAX_TCP_GAP;
-  }
+  if( gap_in_regs < MAX_RTU_GAP ) gap_in_regs_opt = MAX_RTU_GAP;
+  if( gap_in_regs > MAX_TCP_GAP ) gap_in_regs_opt = MAX_TCP_GAP;
 
   int       req_idx    = 0;
   /* Инициализируем первый запрос первым элементом */
@@ -51,12 +36,10 @@ int optimize_modbus_requests( uint16_t         *regs,
 
   for( uint16_t i = 1; i < regs_count; i++ ) {
     uint16_t  next_addr = regs[ i ];
-    // Расстояние (пропуск) между текущим и предыдущим регистром
+    /* Расстояние (пропуск) между текущим и предыдущим регистром */
     uint16_t  gap = next_addr - last_addr - 1;
-
     /* Потенциальный размер нового гипотетического запроса */
     uint32_t  potential_len = next_addr - start_addr + 1;
-
     /* Условие разделения: если дыра слишком большая ИЛИ превышен лимит Modbus на один запрос */
     if( gap > gap_in_regs || potential_len > MODBUS_MAX_REGS ) {
       /* Сохраняем текущий сформированный запрос */
@@ -66,17 +49,14 @@ int optimize_modbus_requests( uint16_t         *regs,
       /* Начинаем новый запрос с текущего регистра */
       start_addr = next_addr;
     }
-
     /* Обновляем последний обработанный адрес */
     last_addr = next_addr;
-
     /* Останавливаем анализ, т.к. у нас максимальное число запросов 525 (от 0 до 524) */
     if( req_idx == 524 ) {
       fprintf( stderr, "Достигнуто ограничение по числу запросов, формирование прервано\n" );
       break;
     }
   }
-
   /* Не забываем сохранить самый последний запрос */
   out_requests[ req_idx ].start_addr = start_addr;
   out_requests[ req_idx ].quantity   = last_addr - start_addr + 1;
